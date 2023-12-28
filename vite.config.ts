7aaa488f496ad47d72ac9d8ceac15d28105a7142
Radomir Nowak <kuker16@gmail.com>
2023-12-28 21:42:46 +0100

    single file deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 
+import { viteSingleFile } from "vite-plugin-singlefile"
+
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid(), viteSingleFile()],
   build: {
     outDir: 'build',
   }
